3971ab57c8e0b239d77effa95346d40a06ab18f6
wandasetya <wandasetya04@gmail.com>
2022-06-11 21:49:40 +0700

    lab8

---
+++ koneksi.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Koneksi</title>
+</head>
+
+<body>
+  <?php
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $db = "latihan1";
+  $conn = mysqli_connect($host, $user, $pass, $db);
+  if ($conn == false) {
+    echo "Koneksi ke server gagal.";
+    die();
+  } else echo "Koneksi berhasil";
+  ?>
+
+</body>
+
+</html>
